7982544ea767fa84ea34827ec053ac4fdcb8ffec
huymach91 <huy.mach.webdev@gmail.com>
2021-11-07 09:49:02 +0700

    1 file modified

--- index.ts
+++ index.ts
@@ -66,6 +66,11 @@ class NumericInput {
       if (currentChar === this.optional.separator && this.isRemoveTyping) {
         this.insertSeparator(caret - 2, '');
         caret -= 1;
+        const char = this.element.value.charAt(caret - 2); // this char was empty
+        if (char === '') {
+          this.insertSeparator(caret - 1, '');
+          caret = 0;
+        }
       }
       setTimeout(() => {
         this.element.setSelectionRange(caret, caret);
